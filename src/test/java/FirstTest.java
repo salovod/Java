9500f31f9c97f5f4d6b7c42dbdd92ff5c60a2951
salovod <beliysacred@gmail.com>
2014-10-06 21:51:08 +0300

    first commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,19 @@
-/**
- * Created by Sacred on 06.10.2014.
- */
+import java.util.Scanner;
+
 public class FirstTest {
+
+
+    public static void main(String args[]) throws Exception
+    {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Type number:");
+        String i = read.nextLine();
+        System.out.println("Your number is:" + i);
+        int k = Integer.parseInt(i);
+        int summa = k/100 + ((k-(k/100)*100)/10) + ((k- (k/100)*100) - ((((k- (k/100)*100)/10))*10));
+        System.out.println(summa);
+    }
+
+
+
 }
